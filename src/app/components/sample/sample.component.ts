94a88b41625315233ea4deaead9c861eac8a0686
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-25 17:25:21 +0530

    sample

--- src/app/components/sample/sample.component.ts
+++ src/app/components/sample/sample.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { fromEvent, interval, of, sample } from 'rxjs';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
-  styleUrls: ['./sample.component.scss']
+  styleUrls: ['./sample.component.scss'],
 })
-export class SampleComponent {
+export class SampleComponent implements AfterViewInit {
+  constructor() {}
+  ngAfterViewInit(): void {
+    let btn$ = fromEvent(document.getElementById('now')!, 'click');
+    let interval$ = interval(1000);
+    interval$
+      .pipe(sample(btn$))
 
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          console.log('complete');
+        }
+      );
+  }
 }
